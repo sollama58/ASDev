4947326c023a917fb0f7b1d2fa2a05d2b27e76ae
Sollama <rtrdgcr@gmail.com>
2026-01-13 15:49:36 -0500

    ENV

--- src/config/env.js
+++ src/config/env.js
@@ -14,7 +14,7 @@ if (missingVars.length > 0) {
 
 const config = {
     // Server
-    VERSION: "v25.41-KOTH-FALLBACK",
+    VERSION: "v25.42-KOTH-JSON-FIX",
     PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
 
